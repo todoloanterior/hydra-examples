c86fb6e4b49bbd682413f3862587909fa368573f
todoloanterior <57926417+todoloanterior@users.noreply.github.com>
2021-09-18 17:09:46 -0500

    Update crrdrrxburdo.js

--- crrdrrxburdo.js
+++ crrdrrxburdo.js
@@ -1,15 +1,4 @@
-// VORTEX
-
-shape(200, 2, 0.3)
-.modulateScrollY(noise(0.1,0.5).modulate(noise(0.2,0.6)))
-.modulateScrollX(noise(0.5,0.1).modulate(noise(0.6,0.2)))
-.mult(osc(6,0.1,1))
-//.repeat(2,2)
-.blend(o1,0.9)
-.rotate(-0.01)
-.modulateScale(src(o1), 0.01)
-.out(o1)
-
+/////
 src(o1)
 .layer(src(o1))
 .scale(1.0001)
@@ -157,17 +146,17 @@ src(s0)
 
 //8
 
-osc(10, 0.001, () => 0.3 * a.fft[0]*2)
+osc(1, 0.001, () => 0.3 * a.fft[0]*2)
   .rotate(0, 0.2)
   .modulateScale(osc(5, -1, 1.5))
-  .blend(src(s0), 0.9)
-  .modulate(noise(10, 0.7).scrollX(0, 0.4), 0.05)
-  .modulateHue(o0, () => a.fft[2]*30)
+  .blend(src(s0), 0.99)
+  .modulate(noise(2, 0.07).scrollX(0, 0), 0.002)
+  //.modulateHue(o0, () => a.fft[0]*3)
   .saturate(1.1)
-  .colorama()
-  .scale(0.99)
+  .colorama(0.01)
+//.layer(src(s0).saturate(1.1).luma().scale(1.02).scrollX(0,0), 0.8)
+  .scale(0.999)
   .out()
-
     
 
  
