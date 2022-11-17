c15a1a4ba84285cb6dfbf424add1bf7899ab43fb
todoloanterior <57926417+todoloanterior@users.noreply.github.com>
2022-11-16 21:33:41 -0500

    Update and rename corredorrxburdo.js to LA HERMANADAD 2.js

--- LA HERMANADAD 2.js
+++ LA HERMANADAD 2.js
@@ -121,8 +121,34 @@ osc(1, 0.001, () => 0.3 * a.fft[0]*2)
 //.layer(src(s0).saturate(1.1).luma().scale(1.02).scrollX(0,0), 0.8)
   .scale(0.999)
   .out()
+
+
+/////edgess /////////
+
+
+osc(100, .1, [0,6])
+  .mult(shape(1, () => a.fft[0]*0.2))
+  .out(o1)
+src(o0)
+  .modulateHue(src(o0).scale(1.01),1.1)
+  //.layer(src(o1).luma(.1, .03).scrollX( [.25,-.25]).scrollY(0))
+  .scrollY([0, 0, -0.001])
+  .scrollX(.001)
+  .contrast(2.1)
+  //.scale([1, 1, 1, 1, 1,1,1,1, 1.1])
+  .out()
     
 
- 
+ ///////// faded //////////////////
+
+
+src(o0)
+.rotate(0,0.1)
+.add(shape([2,3,4,5])
+.scale(1,0.2,1)
+.scrollX(0.3,-0.31)
+.rotate(0,0.03))
+.add(shape(3).scale(1,0.3,0.1).color(0.1,0.8,1).scrollX(-0.3,0.02)).rotate(0, -0.05).modulateHue(src(o0).scale(1,0.1),1).layer(osc(0.9,0.8,0.2).mask(shape([2,4,6,8,7,4,3],0.007,0.02))).rotate(0,0.01).modulate(voronoi(101)).modulate(osc(1,1)).mask(shape(4)).modulate(osc(1000))
+.out()
 
   
